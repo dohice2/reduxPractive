b29c24de9c1d2389e357f91917c1bd66764bea54
Dora Cervantes <dcerva1@discover.com>
2020-02-05 12:32:40 -0600

    something new

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
- import counterReducer from './counter';
- import loggedReducer from './isLogged';
+import counterReducer from './counter';
+import loggedReducer from './isLogged';
 import {combineReducers} from 'redux';
 
-const allReducers= combineReducers({
-    counter :counterReducer,
+const allReducers = combineReducers({
+    counter : counterReducer,
     isLogged: loggedReducer
 });
 
